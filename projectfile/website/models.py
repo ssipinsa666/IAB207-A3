b683db2d8fa5c882726551752044926862c15c83
ssipinsa666 <n10661531@qut.edu.au>
2024-05-17 06:06:48 +1000

    Leo's Data Model

--- projectfile/website/models.py
+++ projectfile/website/models.py
@@ -3,9 +3,57 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, index=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    contact_number = db.Column(db.Integer, nullable=False)
+    events = db.relationship('Event', backref='organizer')
+    comments = db.relationship('Comment', backref='author')
+    orders = db.relationship('Order', backref='buyer')
+
+    # string print method
+    def __repr__(self):
+        return f"Name: {self.username}"
 
 class Event(db.Model):
+    __tablename__ = 'event'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    location = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    image = db.Column(db.String(400), nullable=False)
+    status = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    organizer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comments = db.relationship('Comment', backref='event')
+    orders = db.relationship('Order', backref='event')
+
+    # string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.now())
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+
+    # string print method
+    def __repr__(self):
+        return f"Comment: {self.content}"
 
 class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    buyer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    date_ordered = db.Column(db.DateTime, default=datetime.now())
+
+    
